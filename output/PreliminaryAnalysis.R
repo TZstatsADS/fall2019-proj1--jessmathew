64f1ec35a8d95e2dd9c5c87f2ff1da8ffb211f12
jessmathew <51378669+jessmathew@users.noreply.github.com>
2019-09-10 19:33:33 -0400

    Add files via upload
    Preliminary analysis of the "lyrics" data.

---
+++ output/PreliminaryAnalysis.R
@@ -0,0 +1,4 @@
+setwd("C:\\Users\\jessm\\OneDrive\\Documents\\Columbia University\\Fall 2019\\STAT 5243 - Applied Data Science\\Projects\\Project 1\\Data")
+lyricsData <- get(load("lyrics.RData")) 
+head(lyricsData)
+
